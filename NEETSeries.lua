7df2a401df495e8f2ebdcef652307428cf38d4db
Ryzuki <vtneet@gmail.com>
2017-01-06 22:21:59 +0700

    Update NEETSeries.lua

--- NEETSeries.lua
+++ NEETSeries.lua
@@ -1,4 +1,4 @@
---[[ NEET Series Version 0.246
+--[[ NEET Series Version 0.25
 	_____   ___________________________   ________           _____             
 	___  | / /__  ____/__  ____/__  __/   __  ___/______________(_)____________
 	__   |/ /__  __/  __  __/  __  /      _____ \_  _ \_  ___/_  /_  _ \_  ___/
@@ -6,7 +6,7 @@
 	/_/ |_/  /_____/  /_____/  /_/        /____/ \___//_/    /_/  \___//____/  
 
 ---------------------------------------]]
-local NEETSeries_Version = 0.246
+local NEETSeries_Version = 0.25
 local function NEETSeries_Print(text) PrintChat(string.format("<font color=\"#4169E1\"><b>[NEET Series]:</b></font><font color=\"#FFFFFF\"> %s</font>", tostring(text))) end
 
 if not FileExist(COMMON_PATH.."MixLib.lua") then
@@ -16,19 +16,32 @@ if not FileExist(COMMON_PATH.."MixLib.lua") then
 else require('MixLib') end
 
 if not FileExist(COMMON_PATH.."OpenPredict.lua") or not FileExist(COMMON_PATH.."ChallengerCommon.lua") or not FileExist(COMMON_PATH.."DamageLib.lua") or not FileExist(COMMON_PATH.."Analytics.lua") then return end
-if not ChallengerCommonLoaded then require('ChallengerCommon') end
+if not ChallengerCommonLoaded then require("ChallengerCommon") end
 if not Analytics then require("Analytics") end
 
 local SupTbl = {"Xerath", "KogMaw", "Annie"}
 local Supported = Set(SupTbl)
 
 local NS_Menu = MenuConfig("NEETSeries", "[NEET Series]: Menu")
-	NS_Menu:Boolean("Tracker", "Load Tracker", true, function(v) NEETSeries_Print("Please 2x F6 to "..(v == true and "Load" or "UnLoad").." NS_Awa") end)
+	NS_Menu:Boolean("Tracker", "Load Tracker", true, function(v) NEETSeries_Print("Please 2x F6 to "..(v and "Load" or "UnLoad").." NS_Awa") end)
 	if Supported[myHero.charName] then NS_Menu:Boolean("Plugin", "Load NS_"..myHero.charName, true, function(v) NEETSeries_Print("Please 2x F6 to "..(v == true and "Load" or "UnLoad").." NS_"..myHero.charName) end)
 	else NS_Menu:Info("nope", "Not supported for "..myHero.charName) end
 	NS_Menu:Info("ifo", "Current Orbwalker: "..Mix.OW)
 	NS_Menu:Info("ifo2", "Script Version: "..NEETSeries_Version)
 	NS_Menu:Info("ifo3", "LoL Version: "..GetGameVersion():sub(1, 13))
+
+class "LoadEnemies"
+function LoadEnemies:__init()
+	self.Count = 0;
+	self.List = {nil, nil, nil, nil, nil};
+	OnObjectLoad(function(Object)
+		if Object.team ~= myHero.team and Object.isHero then
+			self.Count = self.Count + 1;
+			self.List[self.Count] = Object;
+		end
+	end)
+	table.sort(self.List, function(a, b) return a.charName < b.charName end)
+end
 
 class "__MinionManager"
 function __MinionManager:__init(range1, range2)
@@ -45,21 +58,35 @@ function __MinionManager:__init(range1, range2)
 end
 
 function __MinionManager:CreateObj(obj)
-	if GetObjectType(obj) == "obj_AI_Minion" and GetTeam(obj) == MINION_ENEMY and GetObjectBaseName(obj):find("Minion_") and IsObjectAlive(obj) then
-		self.minion[GetNetworkID(obj)] = obj
-	elseif GetTeam(obj) == 300 and GetObjectType(obj) == "obj_AI_Minion" and IsObjectAlive(obj) then
-		self.mob[GetNetworkID(obj)] = obj
+	if GetObjectType(obj) ~= "obj_AI_Minion" or GetTeam(obj) == MINION_ALLY then return end
+	if GetObjectBaseName(obj):find("Minion_") and GetTeam(obj) ~= 300 then
+		self.minion[#self.minion + 1] = obj
+		return
 	end
+
+	if GetTeam(obj) == 300 then
+		self.mob[#self.mob + 1] = obj
+	end	
 end
 
 function __MinionManager:DeleteObj(obj)
 	if GetObjectType(obj) ~= "obj_AI_Minion" or GetTeam(obj) == MINION_ALLY then return end
 	if GetObjectBaseName(obj):find("Minion_") and GetTeam(obj) ~= 300 then
-		self.minion[GetNetworkID(obj)] = nil
+		for i = 1, #self.minion do
+			if self.minion[i] == obj then
+				table.remove(self.minion, i)
+				return
+			end
+		end
 	end
 
 	if GetTeam(obj) == 300 then
-		self.mob[GetNetworkID(obj)] = nil
+		for i = 1, #self.mob do
+			if self.mob[i] == obj then
+				table.remove(self.mob, i)
+				return
+			end
+		end
 	end
 end
 
@@ -72,35 +99,77 @@ function __MinionManager:Update()
 	end
 
 	self.tmob = {}
-	self.mmob = nil
+	local BiggestMob = nil
 	for _, mob in pairs(self.mob) do
 		if GetDistanceSqr(mob) <= self.range2 and IsObjectAlive(mob) and IsTargetable(mob) and not IsImmune(mob, myHero) and GetTeam(mob) == 300 then
 			self.tmob[#self.tmob + 1] = mob
-			if not self.mmob or GetMaxHP(self.mmob) < GetMaxHP(mob) then self.mmob = mob end
+			if not BiggestMob or GetMaxHP(BiggestMob) < GetMaxHP(mob) then BiggestMob = mob end
 		end
+	end
+	self.mmob = BiggestMob
+end
+
+local pred = {"OpenPredict", "GPrediction", "GoSPrediction"}
+function LoadPredMenu(menu)
+	menu:DropDown("cpred", "Choose Prediction:", 1, pred, function(val) NEETSeries_Print("2x F6 to ally using "..pred[val]) end)
+	menu:Info("currentPred", "Current Prediction: "..pred[menu.cpred:Value()])
+end
+
+local OPM = {
+	["linear"] = GetLinearAOEPrediction,
+	["circular"] = GetCircularAOEPrediction,
+	["cone"] = GetConicAOEPrediction
+}
+local checkHC = {
+	{0.1, 0.25, 0.4, 0.9},
+	{3, 3, 3, 3},
+	{1, 1, 1, 1}
+}
+class "AddSpell"
+function AddSpell:__init(spellData, menu, v)
+	menu:DropDown("chc", "Choose Hit-chance", 3, {"Low", "Normal", "High", "Very High"}, function(v) self.hc = checkHC[self.method][v] end)
+	self.data = spellData
+	if self.data.type == "line" and v == 1 then self.data.type = "linear" end
+	if self.data.type == "linear" and v == 2 then self.data.type = "line" end
+	self.data.radius = self.data.width * 0.5
+	self.method = v
+	self.hc = checkHC[self.method][menu.chc:Value()]
+end
+
+local function CastTo(pos, slot, css2)
+	if not css2 then
+		CastSkillShot(slot, pos)
+	else
+		if GetDistanceSqr(pos) <= css2*css2 then CastSkillShot2(slot, pos) end
 	end
 end
 
-class "PredictSpell"
-function PredictSpell:__init(Slot, Delay, Speed, Width, Range, Collision, collNum, Aoe, Type, Name, HitChance, predName, Other)
-	local Angle = (Other and Other.angle) and Other.angle or nil
-	local Accel = (Other and Other.accel ) and Other.accel or nil
-	local Min = (Accel and Other.min) and Other.min or nil
-	local Max = (Accel and Other.max) and Other.max or nil
-	self.Pred = predName
-	self.data = { slot = Slot, name = Name, speed = Speed, delay = Delay, range = Range + myHero.boundingRadius, width = Width, radius = Width*0.5, collision = Collision, col = {"minion", "yasuowall"}, coll = collNum, aoe = Aoe, type = Type, hc = HitChance, angle = Angle, accel = Accel, minSpeed = Min, maxSpeed = Max }
-	self.IPrediction = self.Pred == "IPrediction" and IPrediction.Prediction(self.data)
-	self.css2 = (Other and Other.s2) and true or false
-end
-
-function PredictSpell:Cast(target, CSS2Range)
-	if not IsReady(self.data.slot) or not target then self.hc, self.pos = 0, nil return end
-	local HitChance, Pos, CanCast, Name = Mix:Predicting(self.Pred, target, self.data, self.IPrediction)
-	if CanCast and ((Name == "OpenPredict" and HitChance >= self.data.hc) or (Name == "IPrediction" and HitChance > 2) or (Name == "GoSPrediction" and HitChance >= 1) or (Name == "GPrediction" and HitChance > 1)) then
-		if not self.css2 and GetDistance(Pos) <= self.data.range then
-			CastSkillShot(self.data.slot, Pos)
-		elseif self.css2 and GetDistance(Pos) <= CSS2Range then
-			CastSkillShot2(self.data.slot, Pos)
+function AddSpell:Cast(target, CSS2)
+	if self.method == 1 then
+		if self.data.col > 0 then
+			local Pred = GetPrediction(target, self.data)
+			if not Pred:mCollision(self.data.col) and Pred.hitChance >= self.hc then
+				CastTo(Pred.castPos, self.data.slot, CSS2)
+			end
+		else
+			local Pred = OPM[self.data.type](target, self.data)
+			if Pred.hitChance >= self.hc then
+				CastTo(Pred.castPos, self.data.slot, CSS2)
+			end
+		end
+		return
+	end
+	if self.method == 2 then
+		local Pred = gPred:GetPrediction(target, myHero, self.data, self.data.col == 0, self.data.col > 0)
+		if Pred.HitChance >= self.hc then
+			CastTo(Pred.CastPosition, self.data.slot, CSS2)
+		end
+		return
+	end
+	if self.method == 3 then
+		local Pred = GetPredictionForPlayer(myHero, target, target.ms, self.data.speed, self.data.delay, self.data.range, self.data.width, self.data.col > 0, true)
+		if Pred.HitChance >= self.hc then
+			CastTo(Pred.PredPos, self.data.slot, CSS2)
 		end
 	end
 end
@@ -124,7 +193,6 @@ function NS_updateP(v, Ver)
 	if v > #SupTbl then NEETSeries_Print("Updated to version "..Ver..". Please F6 x2 to reload.") return end
 	DownloadFileAsync("https://raw.githubusercontent.com/VTNEETS/GoS/master/NS_"..SupTbl[v]..".lua", COMMON_PATH.."NS_"..SupTbl[v]..".lua", function() NS_updateP(v + 1, Ver) return end) return
 end
-
 
 OnLoad(function()
 	GetWebResultAsync("https://raw.githubusercontent.com/VTNEETS/GoS/master/NEETSeries.version", function(OnlineVer)
@@ -187,5 +255,7 @@ end
 		{ Version 0.246 }
 			- Delete Annie W Laneclear (fps drop)
 
+		{ Version 0.25 }
+			- Improve Fps
 
 -------------------------------------------]]
